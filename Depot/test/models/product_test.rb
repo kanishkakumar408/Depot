ddd8dd1f775ec5aba1096e459bf2cda14700892e
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2020-12-04 16:51:19 +0530

    Unit testing through fixtures

--- Depot/test/models/product_test.rb
+++ Depot/test/models/product_test.rb
@@ -1,20 +1,19 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
- test "product attributes must not be empty" do
+  fixtures :products
+  test "product attributes must not be empty" do
    product = Product.new
    assert product.invalid?
    assert product.errors[:title].any?
    assert product.errors[:description].any?
    assert product.errors[:price].any?
    assert product.errors[:image_url].any?
-end
- test "product price must be positive" do
-   product = Product.new(title:
-                       "My Book Title",
+  end
+  test "product price must be positive" do
+   product = Product.new(title: "My Book Title",
                         description: "yyy",
-                        image_url:
-                        "zzz.jpg")
+                        image_url: "zzz.jpg")
    product.price = -1
    assert product.invalid?
    assert_equal ["must be greater than or equal to 0.01"],
@@ -25,29 +24,45 @@ end
         product.errors[:price]
    product.price = 1
    assert product.valid?
-   end
+  end
 
 
-   def new_product(image_url)
-    Product.new(title:"My Book Title",
+  def new_product(image_url)
+    Product.new(title: "My Book Title",
                 description: "yyy",
-                price:1,
-                image_url:image_url)
-   end
- test "image url" do
-   ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
-            http://a.b.c/x/y/z/fred.gif }
-   bad = %w{ fred.doc fred.gif/more fred.gif.more }
+                price:   1,
+                image_url: image_url)
+  end
 
-   ok.each do |image_url|
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+            http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    ok.each do |image_url|
       assert new_product(image_url).valid?,
          "#{image_url} shouldn't be invalid"
-end
+  end
    bad.each do |image_url|
       assert new_product(image_url).invalid?,
          "#{image_url} shouldn't be valid"
+  end
+  test "product is not valid without a unique title" do
+    product = Product.new(title:        products(:ruby).title,
+                          description: "yyy",
+                          price:        1,
+                          image_url:    "fred.gif")
+  assert product.invalid?
+  assert_equal ["has already been taken"], product.errors[:title]
+  end
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy",
+                          price: 1,
+                          image_url: "fred.gif")
+   assert product.invalid?
+   assert_equal [I18n.translate('errors.messages.taken')],
+   product.errors[:title]
 end
 end
-
-
 end
