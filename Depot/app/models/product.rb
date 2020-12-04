344ef68f0115e4082b9f25083da605bd3cf92091
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2020-12-04 11:42:04 +0530

    Validation on product model image_url

--- Depot/app/models/product.rb
+++ Depot/app/models/product.rb
@@ -2,5 +2,8 @@ class Product < ApplicationRecord
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greate_than_or_equal_to: 0.01}
 	validates :title , uniqueness: true
-	
+	validates :image_url, allow_blank: true, format: {
+		with:  %r{\.(gif|jpg|png)\z}i,
+		message: 'must be URL for GIF , JPG or PNG format.'
+	}
 end
