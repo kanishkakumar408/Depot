43e2bc59ba0f1cdcae050a1c83f6247c182b2b1d
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2020-12-28 14:59:27 +0530

    added before_save callback to capitalize first letter of title

--- bookshop/app/models/product.rb
+++ bookshop/app/models/product.rb
@@ -1,5 +1,10 @@
 class Product < ApplicationRecord
+before_save :titlecapitalize
 
+  def titlecapitalize
+  	self.title = self.title.capitalize
+  end
+  	
   def self.search(search)
 	if search
 	   where(["title LIKE ?","%#{search}%"])
